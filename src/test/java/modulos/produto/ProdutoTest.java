64c17672f1e94e7f17f04f39af4abda6b2b556e6
Luiz Neto <luizitow@hotmail.com>
2022-07-27 01:17:31 -0400

    Removi importacoes e comentarios desnecessarios

--- src/test/java/modulos/produto/ProdutoTest.java
+++ src/test/java/modulos/produto/ProdutoTest.java
@@ -17,7 +17,6 @@ public class ProdutoTest {
     public void beforeEach (){
         // Configurando os dados da API Rest da Lojinha
         baseURI = "http://165.227.93.41";
-        // port = 8080;
         basePath = "/lojinha-bugada";
 
         // Obter o token do usuario admin
